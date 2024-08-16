88069ea02532afbe55b59a58986080241b34fd34
Aleksandar Stolikj <stolaaar@gmail.com>
2024-08-17 00:26:12 +0200

    feat: solve reverse integer problem

--- problems/7.reverse-integer/reverse-integer.go
+++ problems/7.reverse-integer/reverse-integer.go
@@ -1,3 +1,36 @@
+package main
+
+import "math"
+
 func reverse(x int) int {
-    
+	reversed, negative := 0, x < 0
+
+	if negative {
+		x = int(math.Abs(float64(x)))
+	}
+
+	for x > 0 {
+		rm := x % 10
+		reversed = (reversed * 10) + rm
+		x = x / 10
+	}
+
+	if negative {
+		reversed = reversed - (2 * reversed)
+	}
+
+	if reversed > math.MaxInt32 && !negative {
+		return 0
+	}
+
+	if reversed < math.MinInt32 {
+		return 0
+	}
+
+	return reversed
+}
+
+func main() {
+	println(reverse(2147483649))
+	println(reverse(-2331))
 }
